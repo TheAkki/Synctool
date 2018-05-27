44186943987fd5e6b0e54aaaa1c59fc76ccac59a
theakki <theakki@mailbox.org>
2018-05-27 16:36:51 +0200

    Extent Testcases

--- app/src/test/java/theakki/synctool/FileItemHelper_FillMergeList_Test.java
+++ app/src/test/java/theakki/synctool/FileItemHelper_FillMergeList_Test.java
@@ -13,66 +13,169 @@ import theakki.synctool.Job.Merger.MergeResult;
 
 public class FileItemHelper_FillMergeList_Test
 {
-    private boolean isEqualWithoutFlag(ArrayList<FileMergeResult> expected, ArrayList<FileMergeResult> actual)
-    {
-        if(expected.size() != actual.size())
-            return false;
-
-        for(int i = 0; i < expected.size(); ++i)
-        {
-            if(expected.get(i).isEqual(actual.get(i), true) == false)
-                return false;
-        }
-
-        return true;
-    }
-
     @Test
-    public void merge_IsEqualNoSkipEqual() throws Exception
+    public void fill_IsEqual_A2B_NoSkipEqual() throws Exception
     {
         FileItem file1A = new FileItem("file1.txt", "/path1/", 12345, 987654321);
         FileItem file1B = new FileItem("file1.txt", "/path1/", 12345, 987654321);
 
-        final ArrayList<FileItem> listA_AtoB = new ArrayList<>(Arrays.asList( file1A ));
-        final ArrayList<FileItem> listA_BtoA = FileItemHelper.clone( listA_AtoB );
-        final ArrayList<FileItem> listB_AtoB = new ArrayList<>(Arrays.asList( file1B ));
-        final ArrayList<FileItem> listB_BtoA =  FileItemHelper.clone( listB_AtoB );
+        final ArrayList<FileItem> listA = new ArrayList<>(Arrays.asList( file1A ));
+        final ArrayList<FileItem> listB = new ArrayList<>(Arrays.asList( file1B ));
 
-        final ArrayList<FileMergeResult> expectedAtoB = new ArrayList<>(Arrays.asList( new FileMergeResult(MergeResult.MatchExactly, file1A, file1B) ));
-        final ArrayList<FileMergeResult> expectedBtoA = new ArrayList<>(Arrays.asList( new FileMergeResult(MergeResult.MatchExactly, file1B, file1A) ));
+        /* Expected Result: We found exactly the needed file */
+        final ArrayList<FileMergeResult> expected = new ArrayList<>(Arrays.asList( new FileMergeResult(MergeResult.MatchExactly, file1A.clone(), file1B.clone()) ));
+        ArrayList<FileMergeResult> actual= new ArrayList<>();
 
-        ArrayList<FileMergeResult> actualAtoB = new ArrayList<>();
-        ArrayList<FileMergeResult> actualBtoA = new ArrayList<>();
-
-        FileItemHelper.fillMergeList(listA_AtoB, listB_AtoB, actualAtoB, false, false, true);
-        assertTrue( isEqualWithoutFlag(expectedAtoB, actualAtoB) );
-
-        FileItemHelper.fillMergeList(listA_BtoA, listB_BtoA, actualBtoA, false, false, false);
-        assertTrue( isEqualWithoutFlag(expectedBtoA, actualBtoA) );
+        FileItemHelper.fillMergeList(listA, listB, actual, false, false, true, false);
+        FileItemHelper_Test.resetFlagMergeResult(actual);
+        assertEquals(expected, actual);
     }
 
+
     @Test
-    public void merge_IsEqualSkipEqual() throws Exception
+    public void fill_IsEqual_B2A_NoSkipEqual() throws Exception
     {
         FileItem file1A = new FileItem("file1.txt", "/path1/", 12345, 987654321);
         FileItem file1B = new FileItem("file1.txt", "/path1/", 12345, 987654321);
 
-        final ArrayList<FileItem> listA_AtoB = new ArrayList<>(Arrays.asList( file1A ));
-        final ArrayList<FileItem> listA_BtoA = FileItemHelper.clone( listA_AtoB );
-        final ArrayList<FileItem> listB_AtoB = new ArrayList<>(Arrays.asList( file1B ));
-        final ArrayList<FileItem> listB_BtoA =  FileItemHelper.clone( listB_AtoB );
+        final ArrayList<FileItem> listA = new ArrayList<>(Arrays.asList( file1A ));
+        final ArrayList<FileItem> listB = new ArrayList<>(Arrays.asList( file1B ));
 
-        final ArrayList<FileMergeResult> expectedAtoB = new ArrayList<>();
-        final ArrayList<FileMergeResult> expectedBtoA = new ArrayList<>();
+        /* Expected Result: We found exactly the needed file */
+        final ArrayList<FileMergeResult> expected = new ArrayList<>(Arrays.asList( new FileMergeResult(MergeResult.MatchExactly, file1B.clone(), file1A.clone()) ));
+        ArrayList<FileMergeResult> actual= new ArrayList<>();
 
-        ArrayList<FileMergeResult> actualAtoB = new ArrayList<>();
-        ArrayList<FileMergeResult> actualBtoA = new ArrayList<>();
+        FileItemHelper.fillMergeList(listB, listA, actual, false, false, false, false);
+        FileItemHelper_Test.resetFlagMergeResult(actual);
+        assertEquals(expected, actual);
+    }
 
-        FileItemHelper.fillMergeList(listA_AtoB, listB_AtoB, actualAtoB, true, false, true);
-        assertTrue( isEqualWithoutFlag(expectedAtoB, actualAtoB) );
 
-        FileItemHelper.fillMergeList(listA_BtoA, listB_BtoA, actualBtoA, true, false, false);
-        assertTrue( isEqualWithoutFlag(expectedBtoA, actualBtoA) );
+    @Test
+    public void fill_IsEqual_A2B_SkipEqual() throws Exception
+    {
+        FileItem file1A = new FileItem("file1.txt", "/path1/", 12345, 987654321);
+        FileItem file1B = new FileItem("file1.txt", "/path1/", 12345, 987654321);
+
+        final ArrayList<FileItem> listA = new ArrayList<>(Arrays.asList( file1A ));
+        final ArrayList<FileItem> listB = new ArrayList<>(Arrays.asList( file1B ));
+
+        /* Expected Result: We found nothing. Because we skip equal */
+        final ArrayList<FileMergeResult> expected = new ArrayList<>();
+        ArrayList<FileMergeResult> actual= new ArrayList<>();
+
+        FileItemHelper.fillMergeList(listA, listB, actual, true, false, true, false);
+        FileItemHelper_Test.resetFlagMergeResult(actual);
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void fill_IsEqual_B2A_SkipEqual() throws Exception
+    {
+        FileItem file1A = new FileItem("file1.txt", "/path1/", 12345, 987654321);
+        FileItem file1B = new FileItem("file1.txt", "/path1/", 12345, 987654321);
+
+        final ArrayList<FileItem> listA = new ArrayList<>(Arrays.asList( file1A ));
+        final ArrayList<FileItem> listB = new ArrayList<>(Arrays.asList( file1B ));
+
+        /* Expected Result: We found nothing. Because we skip equal */
+        final ArrayList<FileMergeResult> expected = new ArrayList<>();
+        ArrayList<FileMergeResult> actual= new ArrayList<>();
+
+        FileItemHelper.fillMergeList(listB, listA, actual, true, false, false, false);
+        FileItemHelper_Test.resetFlagMergeResult(actual);
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void fill_Moved_A2B() throws Exception
+    {
+        FileItem file1A = new FileItem("file1.txt", "/path1/", 12345, 987654321);
+        FileItem file1B = new FileItem("file1.txt", "/path1/path2/", 12345, 987654321);
+
+        final ArrayList<FileItem> listA = new ArrayList<>(Arrays.asList( file1A ));
+        final ArrayList<FileItem> listB = new ArrayList<>(Arrays.asList( file1B ));
+
+        /*  A is master. So B is moved.
+            FileA: B
+            FileB: A
+         */
+        final ArrayList<FileMergeResult> expected = new ArrayList<>(Arrays.asList( new FileMergeResult(MergeResult.Moved_B, file1B.clone(), file1A.clone()) ));
+        ArrayList<FileMergeResult> actual= new ArrayList<>();
+
+        FileItemHelper.fillMergeList(listA, listB, actual, false, false, true, false);
+        FileItemHelper_Test.resetFlagMergeResult(actual);
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void fill_Moved_B2A() throws Exception
+    {
+        FileItem file1A = new FileItem("file1.txt", "/path1/", 12345, 987654321);
+        FileItem file1B = new FileItem("file1.txt", "/path1/path2/", 12345, 987654321);
+
+        final ArrayList<FileItem> listA = new ArrayList<>(Arrays.asList( file1A ));
+        final ArrayList<FileItem> listB = new ArrayList<>(Arrays.asList( file1B ));
+
+        /*  B is master. So A is moved.
+            FileA: A
+            FileB: B
+         */
+        final ArrayList<FileMergeResult> expected = new ArrayList<>(Arrays.asList( new FileMergeResult(MergeResult.Moved_A, file1A.clone(), file1B.clone()) ));
+        ArrayList<FileMergeResult> actual= new ArrayList<>();
+
+        FileItemHelper.fillMergeList(listB, listA, actual, false, false, false, false);
+        FileItemHelper_Test.resetFlagMergeResult(actual);
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void fill_Renamed_A2B() throws Exception
+    {
+        FileItem file1A = new FileItem("file1.txt", "/path1/", 12345, 987654321);
+        FileItem file1B = new FileItem("file2.txt", "/path1/", 12345, 987654321);
+
+        final ArrayList<FileItem> listA = new ArrayList<>(Arrays.asList( file1A ));
+        final ArrayList<FileItem> listB = new ArrayList<>(Arrays.asList( file1B ));
+
+        /**
+         *  A is Master. So rename B
+         *  FileA: B
+         *  FileB: A
+         */
+        final ArrayList<FileMergeResult> expected = new ArrayList<>(Arrays.asList( new FileMergeResult(MergeResult.Renamed_B, file1B.clone(), file1A.clone()) ));
+        ArrayList<FileMergeResult> actual= new ArrayList<>();
+
+        FileItemHelper.fillMergeList(listA, listB, actual, false, false, true, false);
+        FileItemHelper_Test.resetFlagMergeResult(actual);
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void fill_Renamed_B2A() throws Exception
+    {
+        FileItem file1A = new FileItem("file1.txt", "/path1/", 12345, 987654321);
+        FileItem file1B = new FileItem("file2.txt", "/path1/", 12345, 987654321);
+
+        final ArrayList<FileItem> listA = new ArrayList<>(Arrays.asList( file1A ));
+        final ArrayList<FileItem> listB = new ArrayList<>(Arrays.asList( file1B ));
+
+        /**
+         *  B is Master. So rename A
+         *  FileA: A
+         *  FileB: B
+         */
+        final ArrayList<FileMergeResult> expected = new ArrayList<>(Arrays.asList( new FileMergeResult(MergeResult.Renamed_A, file1A.clone(), file1B.clone()) ));
+        ArrayList<FileMergeResult> actual= new ArrayList<>();
+
+        FileItemHelper.fillMergeList(listB, listA, actual, false, false, false, false);
+        FileItemHelper_Test.resetFlagMergeResult(actual);
+        assertEquals(expected, actual);
     }
 
 
--- app/src/test/java/theakki/synctool/FileItemHelper_MergeFileList_Test.java
+++ app/src/test/java/theakki/synctool/FileItemHelper_MergeFileList_Test.java
@@ -25,16 +25,17 @@ public class FileItemHelper_MergeFileList_Test
         final ArrayList<FileItem> listA = new ArrayList<>(Arrays.asList( file1A ));
         final ArrayList<FileItem> listB = new ArrayList<>(Arrays.asList( file1B ));
 
-        // Da A der Master ist muss A verschoben sein.
-        final ArrayList<FileMergeResult> expected = new ArrayList<>(Arrays.asList( new FileMergeResult(MergeResult.Moved_A, file1A.clone(), file1B.clone()) ));
+        // Da A der Master ist muss A verschoben sein und damit B verschoben werden.
+        final ArrayList<FileMergeResult> expected = new ArrayList<>(Arrays.asList( new FileMergeResult(MergeResult.Moved_B, file1B.clone(), file1A.clone()) ));
         ArrayList<FileMergeResult> actual = FileItemHelper.mergeFileList(listA, listB, SyncDirection.ToB, OneWayStrategy.Standard);
         FileItemHelper_Test.resetFlagMergeResult(actual);
 
         assertEquals(expected, actual);
     }
 
+
     @Test
-    public void merge_MovedSingleB2AStandard() throws Exception
+    public void merge_MovedSingle_B2A_Standard() throws Exception
     {
         FileItem file1A = new FileItem("file1.txt", "/path1/", 12345, 987654321);
         FileItem file1B = new FileItem("file1.txt", "/path1/path2/", 12345, 987654321);
@@ -42,15 +43,17 @@ public class FileItemHelper_MergeFileList_Test
         final ArrayList<FileItem> listA = new ArrayList<>(Arrays.asList( file1A ));
         final ArrayList<FileItem> listB = new ArrayList<>(Arrays.asList( file1B ));
 
-        final ArrayList<FileMergeResult> expected = new ArrayList<>(Arrays.asList( new FileMergeResult(MergeResult.Moved_B, file1A.clone(), file1B.clone()) ));
+        // B ist Master. Damit muss A verschoben werden.
+        final ArrayList<FileMergeResult> expected = new ArrayList<>(Arrays.asList( new FileMergeResult(MergeResult.Moved_A, file1A.clone(), file1B.clone()) ));
         ArrayList<FileMergeResult> actual = FileItemHelper.mergeFileList(listA, listB, SyncDirection.ToA, OneWayStrategy.Standard);
         FileItemHelper_Test.resetFlagMergeResult(actual);
 
         assertEquals(expected, actual);
     }
 
+
     @Test
-    public void merge_EqualSingleA2BStandard() throws Exception
+    public void merge_EqualSingle_A2B_Standard() throws Exception
     {
         FileItem file1A = new FileItem("file1.txt", "/path1/", 12345, 987654321);
         FileItem file1B = new FileItem("file1.txt", "/path1/", 12345, 987654321);
@@ -66,8 +69,9 @@ public class FileItemHelper_MergeFileList_Test
         assertEquals(expected, actual);
     }
 
+
     @Test
-    public void merge_EqualSingleB2AStandard() throws Exception
+    public void merge_EqualSingle_B2A_Standard() throws Exception
     {
         FileItem file1A = new FileItem("file1.txt", "/path1/", 12345, 987654321);
         FileItem file1B = new FileItem("file1.txt", "/path1/", 12345, 987654321);
@@ -83,8 +87,9 @@ public class FileItemHelper_MergeFileList_Test
         assertEquals(expected, actual);
     }
 
+
     @Test
-    public void merge_AlreadySyncA2B_NewDateFolder() throws Exception
+    public void merge_AlreadySync_A2B_NewDateFolder() throws Exception
     {
         FileItem file1A = new FileItem("file1.txt", "/path1/", 12345, 987654321);
         FileItem file1B = new FileItem("file1.txt", "/path1/Date1/", 12345, 987654321);
@@ -92,7 +97,7 @@ public class FileItemHelper_MergeFileList_Test
         final ArrayList<FileItem> listA = new ArrayList<>(Arrays.asList( file1A ));
         final ArrayList<FileItem> listB = new ArrayList<>(Arrays.asList( file1B ));
 
-        // File allready synced. So nothing to do
+        // File already synced. So nothing to do
         final ArrayList<FileMergeResult> expected = new ArrayList<>();
         ArrayList<FileMergeResult> actual = FileItemHelper.mergeFileList(listA, listB, SyncDirection.ToB, OneWayStrategy.NewFilesInDateFolder);
         FileItemHelper_Test.resetFlagMergeResult(actual);
@@ -100,8 +105,9 @@ public class FileItemHelper_MergeFileList_Test
         assertEquals(expected, actual);
     }
 
+
     @Test
-    public void merge_NotSyncSyncA2B_NewDateFolder() throws Exception
+    public void merge_NotSyncSync_A2B_NewDateFolder() throws Exception
     {
         FileItem file1A = new FileItem("file1.txt", "/path1/", 12345, 987654321);
         FileItem file1B = new FileItem("file11.txt", "/path1/Date1/", 12345, 987654321);
@@ -109,9 +115,45 @@ public class FileItemHelper_MergeFileList_Test
         final ArrayList<FileItem> listA = new ArrayList<>(Arrays.asList( file1A ));
         final ArrayList<FileItem> listB = new ArrayList<>(Arrays.asList( file1B ));
 
-        // Nothing to do when equal
+        // This file is new
         final ArrayList<FileMergeResult> expected = new ArrayList<>(Arrays.asList( new FileMergeResult(MergeResult.NewFile, file1A.clone(), null) ));
         ArrayList<FileMergeResult> actual = FileItemHelper.mergeFileList(listA, listB, SyncDirection.ToB, OneWayStrategy.NewFilesInDateFolder);
+        FileItemHelper_Test.resetFlagMergeResult(actual);
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void merge_NotSyncSync_A2B_AllDateFolder() throws Exception
+    {
+        FileItem file1A = new FileItem("file1.txt", "/path1/", 12345, 987654321);
+        FileItem file1B = new FileItem("file11.txt", "/path1/Date1/", 12345, 987654321);
+
+        final ArrayList<FileItem> listA = new ArrayList<>(Arrays.asList( file1A ));
+        final ArrayList<FileItem> listB = new ArrayList<>(Arrays.asList( file1B ));
+
+        // This file is new
+        final ArrayList<FileMergeResult> expected = new ArrayList<>(Arrays.asList( new FileMergeResult(MergeResult.NewFile, file1A.clone(), null) ));
+        ArrayList<FileMergeResult> actual = FileItemHelper.mergeFileList(listA, listB, SyncDirection.ToB, OneWayStrategy.AllFilesInDateFolder);
+        FileItemHelper_Test.resetFlagMergeResult(actual);
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void merge_AlreadySync_A2B_AllDateFolder() throws Exception
+    {
+        FileItem file1A = new FileItem("file1.txt", "/path1/", 12345, 987654321);
+        FileItem file1B = new FileItem("file1.txt", "/path1/Date1/", 12345, 987654321);
+
+        final ArrayList<FileItem> listA = new ArrayList<>(Arrays.asList( file1A ));
+        final ArrayList<FileItem> listB = new ArrayList<>(Arrays.asList( file1B ));
+
+        // File already synced. So nothing to do
+        final ArrayList<FileMergeResult> expected = new ArrayList<>(Arrays.asList( new FileMergeResult(MergeResult.NewFile, file1A.clone(), null) ));
+        ArrayList<FileMergeResult> actual = FileItemHelper.mergeFileList(listA, listB, SyncDirection.ToB, OneWayStrategy.AllFilesInDateFolder);
         FileItemHelper_Test.resetFlagMergeResult(actual);
 
         assertEquals(expected, actual);
