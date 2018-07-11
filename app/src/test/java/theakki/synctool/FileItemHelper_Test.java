113c51cf60118729dc6ac55e8090cde35ad7c058
theakki <theakki@mailbox.org>
2018-07-11 21:37:46 +0200

    Add some tests...

--- app/src/test/java/theakki/synctool/FileItemHelper_Test.java
+++ app/src/test/java/theakki/synctool/FileItemHelper_Test.java
@@ -1,8 +1,12 @@
 package theakki.synctool;
 
+import android.support.annotation.NonNull;
+
 import org.junit.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -114,5 +118,175 @@ public class FileItemHelper_Test
         final String actual = FileItemHelper.getRelativePath(TestPath, FileName, RemotePath);
         assertEquals(RelativePath, actual);
     }
+
+
+    @Test
+    public void convertFromObjectArray_CallWithNull () throws Exception
+    {
+        final ArrayList<FileItem> actual = FileItemHelper.convertFromObjectArray(null);
+        final ArrayList<FileItem> expected = new ArrayList<>();
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void convertFromObjectArray_CallEmpty() throws Exception
+    {
+        final ArrayList<FileItem> actual = FileItemHelper.convertFromObjectArray(new ArrayList<Object>());
+        final ArrayList<FileItem> expected = new ArrayList<>();
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void convertFromObjectArray_CallValid() throws Exception
+    {
+        final FileItem item1 = new FileItem("Item1", "Folder1", 123, 567);
+        final FileItem item2 = new FileItem("Item2", "Folder2", 1234, 5678);
+
+        final Object[] input = {item1, item2};
+        final ArrayList<Object> inputlist = new ArrayList<>(Arrays.asList(input));
+        final ArrayList<FileItem> actual = FileItemHelper.convertFromObjectArray(inputlist);
+
+
+        final FileItem[] expectedArray = {item1, item2};
+        final ArrayList<FileItem> expected = new ArrayList<>(Arrays.asList(expectedArray));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void clone_CallNull() throws Exception
+    {
+        final ArrayList<FileItem> actual = FileItemHelper.clone(null);
+        final ArrayList<FileItemHelper> expected = new ArrayList<>();
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void clone_CallEmpty() throws Exception
+    {
+        final ArrayList<FileItem> inputList = new ArrayList<>();
+        final ArrayList<FileItem> actual = FileItemHelper.clone(inputList);
+
+        assertNotSame(inputList, actual);
+        assertEquals(inputList, actual);
+        assertTrue(isEqual(inputList, actual));
+    }
+
+
+    @Test
+    public void clone_CallValid() throws Exception
+    {
+        final FileItem item1 = new FileItem("Item1", "Folder1", 123, 567);
+        final FileItem item2 = new FileItem("Item2", "Folder2", 1234, 5678);
+
+        final FileItem[] input = {item1, item2};
+        final ArrayList<FileItem> inputList = new ArrayList<>(Arrays.asList(input));
+
+        final ArrayList<FileItem> actual = FileItemHelper.clone(inputList);
+
+        assertNotSame(inputList, actual);
+        assertEquals(inputList, actual);
+        assertTrue(isEqual(inputList, actual));
+    }
+
+
+
+    public boolean isEqual(@NonNull ArrayList<FileItem> list1, @NonNull ArrayList<FileItem> list2)
+    {
+        if(list1.size() != list2.size())
+            return false;
+
+        for(int i = 0; i < list1.size(); ++i)
+        {
+            final FileItem item1 = list1.get(i);
+            final FileItem item2 = list2.get(i);
+
+            if(item1.isEqual(item2, false, false) == false)
+                return false;
+        }
+
+        return true;
+    }
+
+
+    @Test
+    public void concatPath_CallNull() throws Exception
+    {
+        final String actual = FileItemHelper.concatPath(null);
+        final String expected = "";
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void concatPath_CallEmpty() throws Exception
+    {
+        final String[] input = {};
+        final String actual = FileItemHelper.concatPath(input);
+        final String expected = "/";
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void concatPath_CallValid() throws Exception
+    {
+        final String folder1 = "folder1";
+        final String folder2 = "folder2";
+
+        final String[] input = {folder1, folder2};
+        final String actual = FileItemHelper.concatPath(input);
+        final String expected = "/" + folder1 + "/" + folder2;
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void splitPath_CallNull() throws Exception
+    {
+        final String[] actual = FileItemHelper.splittPath(null);
+        final String[] expected = {};
+
+        assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void splitPath_CallEmpty() throws Exception
+    {
+        final String[] actual = FileItemHelper.splittPath("");
+        final String[] expected = {""};
+
+        assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void splitPath_CallValid() throws Exception
+    {
+        final String folder1 = "f1";
+        final String folder2 = "f2";
+        final String[] expected = {folder1, folder2};
+
+        final String input1 = "/" + folder1 + "/" + folder2;
+        final String[] actual1 = FileItemHelper.splittPath(input1);
+        final String input2 = "/" + folder1 + "/" + folder2 + "/";
+        final String[] actual2 = FileItemHelper.splittPath(input2);
+
+
+
+        assertArrayEquals(expected, actual1);
+        assertArrayEquals(expected, actual2);
+    }
+
 
 }
